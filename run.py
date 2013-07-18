fc070eb7b75a00e0c350d2ba8323e102be45ef80
Matteus <matteus92@gmail.com>
2013-07-18 13:59:51 +0200

    MPD added + GPIO bouncetime

--- run.py
+++ run.py
@@ -1,28 +1,23 @@
-# from mpd import MPDClient
+from mpd import MPDClient
+import 	RPi.GPIO as GPIO
 
-# ip_addr = "127.0.0.1"
-# port = 6600;
+ip_addr = "127.0.0.1"
+port = 6600;
 
-# client = MPDClient()
-# client.timeout = 10
-# client.idletimeout = None
+client = MPDClient()
+client.timeout = 10
+client.idletimeout = None
 
-# print("Connecting.. ")
-# client.connect(ip_addr, port)
-# print("Connected to " + ip_addr + ":" + str(port))
-
-# print(client.next())
-
-# client.close()
-# client.disconnect()
-
-try:
-	import 	RPi.GPIO as GPIO
-except RuntimeError:
-	print("Failed to import RPi.GPIO. Did you run the script with root privileges?")
+print("Connecting.. ")
+client.connect(ip_addr, port)
+print("Connected to " + ip_addr + ":" + str(port))
 
 def gpio_callback(channel):
 	print("Edge detected on channel %s."%channel)
+	if(channel == 16) client.play()
+	elif(channel == 15) client.pause()
+	elif(channel == 13) client.next()
+	elif(channel == 18) client.previous()
 
 GPIO.setmode(GPIO.BOARD)
 
@@ -33,20 +28,22 @@ GPIO.setup(18, GPIO.IN) # Switch 4
 GPIO.setup(22, GPIO.IN) # Switch 5
 GPIO.setup(11, GPIO.IN) # Switch 6
 
-GPIO.add_event_detect(16, GPIO.RISING)
-GPIO.add_event_detect(15, GPIO.RISING)
-GPIO.add_event_detect(13, GPIO.RISING)
-GPIO.add_event_detect(18, GPIO.RISING)
-GPIO.add_event_detect(22, GPIO.RISING)
-GPIO.add_event_detect(11, GPIO.RISING)
+GPIO.add_event_detect(16, GPIO.RISING, callback=gpio_callback, bouncetime=100)
+GPIO.add_event_detect(15, GPIO.RISING, callback=gpio_callback, bouncetime=100)
+GPIO.add_event_detect(13, GPIO.RISING, callback=gpio_callback, bouncetime=100)
+GPIO.add_event_detect(18, GPIO.RISING, callback=gpio_callback, bouncetime=100)
+GPIO.add_event_detect(22, GPIO.RISING, callback=gpio_callback, bouncetime=100)
+GPIO.add_event_detect(11, GPIO.RISING, callback=gpio_callback, bouncetime=100)
 
-GPIO.add_event_callback(16, gpio_callback)
-GPIO.add_event_callback(15, gpio_callback)
-GPIO.add_event_callback(13, gpio_callback)
-GPIO.add_event_callback(18, gpio_callback)
-GPIO.add_event_callback(22, gpio_callback)
-GPIO.add_event_callback(11, gpio_callback)
+# GPIO.add_event_callback(16, gpio_callback)
+# GPIO.add_event_callback(15, gpio_callback)
+# GPIO.add_event_callback(13, gpio_callback)
+# GPIO.add_event_callback(18, gpio_callback)
+# GPIO.add_event_callback(22, gpio_callback)
+# GPIO.add_event_callback(11, gpio_callback)
 
 raw_input("Press enter to stop.")
 
 GPIO.cleanup()
+client.close()
+client.disconnect()
