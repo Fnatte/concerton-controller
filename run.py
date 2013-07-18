982aebbbafc7c6b74b21902f14d9baebe09b5b65
Matteus <matteus92@gmail.com>
2013-07-18 13:29:28 +0200

    GPIO Test

--- run.py
+++ run.py
@@ -1,17 +1,49 @@
-from mpd import MPDClient
+# from mpd import MPDClient
 
-ip_addr = "127.0.0.1"
-port = 6600;
+# ip_addr = "127.0.0.1"
+# port = 6600;
 
-client = MPDClient()
-client.timeout = 10
-client.idletimeout = None
+# client = MPDClient()
+# client.timeout = 10
+# client.idletimeout = None
 
-print("Connecting.. ")
-client.connect(ip_addr, port)
-print("Connected to " + ip_addr + ":" + str(port))
+# print("Connecting.. ")
+# client.connect(ip_addr, port)
+# print("Connected to " + ip_addr + ":" + str(port))
 
-print(client.next())
+# print(client.next())
 
-client.close()
-client.disconnect()
+# client.close()
+# client.disconnect()
+
+try:
+	import 	RPi.GPIO as GPIO
+except RuntimeError:
+	print("Failed to import RPi.GPIO. Did you run the script with root privileges?")
+
+def gpio_callback(channel):
+	print("Edge detected on channel %s."%channel)
+
+GPIO.setMode(GPIO.BOARD)
+
+GPIO.setup(16, GPIO.IN) # Switch 1
+GPIO.setup(15, GPIO.IN) # Switch 2
+GPIO.setup(13, GPIO.IN) # Switch 3
+GPIO.setup(18, GPIO.IN) # Switch 4
+GPIO.setup(22, GPIO.IN) # Switch 5
+GPIO.setup(11, GPIO.IN) # Switch 6
+
+GPIO.add_event_detect(16, GPIO.RISING)
+GPIO.add_event_detect(15, GPIO.RISING)
+GPIO.add_event_detect(13, GPIO.RISING)
+GPIO.add_event_detect(18, GPIO.RISING)
+GPIO.add_event_detect(22, GPIO.RISING)
+GPIO.add_event_detect(11, GPIO.RISING)
+
+GPIO.add_event_callback(16, gpio_callback)
+GPIO.add_event_callback(15, gpio_callback)
+GPIO.add_event_callback(13, gpio_callback)
+GPIO.add_event_callback(18, gpio_callback)
+GPIO.add_event_callback(22, gpio_callback)
+GPIO.add_event_callback(11, gpio_callback)
+
