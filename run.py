359fa0924d65c04495f47791448c3f82e6879826
Matteus <matteus92@gmail.com>
2013-07-18 14:59:03 +0200

    Changed GPIO numbering scheme.

--- run.py
+++ run.py
@@ -27,21 +27,21 @@ def gpio_callback(channel, val):
 	elif(channel == 18):
 		client.previous()
 
-RPIO.setmode(RPIO.BOARD)
+# RPIO.setmode(RPIO.BOARD)
 
-RPIO.setup(16, RPIO.IN) # Switch 1
-RPIO.setup(15, RPIO.IN) # Switch 2
-RPIO.setup(13, RPIO.IN) # Switch 3
-RPIO.setup(18, RPIO.IN) # Switch 4
-RPIO.setup(22, RPIO.IN) # Switch 5
-RPIO.setup(11, RPIO.IN) # Switch 6
+RPIO.setup(23, RPIO.IN) # Switch 1
+RPIO.setup(22, RPIO.IN) # Switch 2
+RPIO.setup(27, RPIO.IN) # Switch 3
+RPIO.setup(24, RPIO.IN) # Switch 4
+RPIO.setup(25, RPIO.IN) # Switch 5
+RPIO.setup(17, RPIO.IN) # Switch 6
 
-RPIO.add_interrupt_callback(16, gpio_callback, edge='rising', debounce_timeout_ms=200)
-RPIO.add_interrupt_callback(15, gpio_callback, edge='rising', debounce_timeout_ms=200)
-RPIO.add_interrupt_callback(13, gpio_callback, edge='rising', debounce_timeout_ms=200)
-RPIO.add_interrupt_callback(18, gpio_callback, edge='rising', debounce_timeout_ms=200)
+RPIO.add_interrupt_callback(23, gpio_callback, edge='rising', debounce_timeout_ms=200)
 RPIO.add_interrupt_callback(22, gpio_callback, edge='rising', debounce_timeout_ms=200)
-RPIO.add_interrupt_callback(11, gpio_callback, edge='rising', debounce_timeout_ms=200)
+RPIO.add_interrupt_callback(27, gpio_callback, edge='rising', debounce_timeout_ms=200)
+RPIO.add_interrupt_callback(24, gpio_callback, edge='rising', debounce_timeout_ms=200)
+RPIO.add_interrupt_callback(25, gpio_callback, edge='rising', debounce_timeout_ms=200)
+RPIO.add_interrupt_callback(17, gpio_callback, edge='rising', debounce_timeout_ms=200)
 
 # GPIO.add_event_callback(16, gpio_callback)
 # GPIO.add_event_callback(15, gpio_callback)
